2da003d9025595508ed198514e6dff926683f67d
Mohammed reida <reidamohammed3@gmail.com>
2025-01-16 11:46:57 +0100

    create the enseignant class and add the get statistique and CRUD function

---
+++ model/Enseignant.php
@@ -0,0 +1,81 @@
+<?php
+namespace App;
+
+use App\Utilisateur;
+
+require_once dirname(__DIR__) . './vendor/autoload.php'; 
+
+class Enseignant extends Utilisateur{
+
+    protected static string $role = "Enseignant";
+
+
+    public function getRole(){
+        return self::$role;
+    }
+
+
+    public static function getCountMembers(){
+        $role=self::$role;
+        $users =CRUD::select('users','count(*) as count', 'role = ?', [$role]);
+        
+           return $users[0]['count'];
+            
+    }
+
+    public static function getAllMembers(){
+        $role=self::$role;
+        $users =CRUD::select('users', '*', 'role = ?', [$role]);
+        return $users;
+    
+    }
+
+    public static function getTopEnseignants() {
+        $role=self::$role;
+        $courses =CRUD::select('cours c 
+        LEFT JOIN users u ON c.enseignant_id = u.id 
+        LEFT JOIN cours_etudiant c_e ON c.id = c_e.cours_id ',
+        'count(DISTINCT c.id) as count_cours,u.username,count(DISTINCT c_e.etudiant_id) as count_iscription',
+        'u.role = ?
+        GROUP BY u.id
+        ORDER BY count_iscription DESC 
+        LIMIT 3;', [$role]);
+        return $courses;
+
+            $users =CRUD::select('users u', '*', 'u.role = ?', [$role]);
+            return $users;
+    }
+    public function getMyCoursesStatistiques($id) {
+     
+        $courses =CRUD::select(' users u
+        LEFT JOIN cours c ON c.enseignant_id = u.id 
+        LEFT JOIN cours_etudiant c_e ON c.id = c_e.cours_id ',
+        'count(DISTINCT c.id) as count_cours,count(DISTINCT c_e.etudiant_id) as count_iscription',
+        'u.id = ?
+        GROUP BY u.id
+        ORDER BY count_iscription DESC ;', [$id]);
+        return $courses;
+
+    }
+
+    public function getMyCourses($id) {
+     
+        $courses =CRUD::select(' users u
+        LEFT JOIN cours c ON c.enseignant_id = u.id 
+        LEFT JOIN cours_etudiant c_e ON c.id = c_e.cours_id ',
+        'c.title,count(DISTINCT c_e.etudiant_id) as count_iscription',
+        'u.id = ?
+        GROUP BY u.id,c.id
+        ORDER BY count_iscription DESC ;', [$id]);
+        return $courses;
+
+    }
+
+}
+if(isset($_GET["action"]) && $_GET["action"]=="register"){
+    $enseignant = new Enseignant;
+    $role=$enseignant->getRole();
+    $enseignant->register($role);
+}
+
+?>
