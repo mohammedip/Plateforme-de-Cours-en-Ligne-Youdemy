5c401f666a0cfe88e281c79e1bf51c41613c5d3c
Mohammed reida <reidamohammed3@gmail.com>
2025-01-18 22:20:18 +0100

    add the tables tag and cours_tag to the query of the method getMyCourses

--- model/Etudiant.php
+++ model/Etudiant.php
@@ -45,24 +45,62 @@ class Etudiant extends Utilisateur{
      
         $courses =CRUD::select(' users u
         JOIN cours_etudiant c_e ON u.id = c_e.etudiant_id
-         JOIN cours c ON c_e.cours_id = c.id ',
-        'c.*,c_e.status',
+         JOIN cours c ON c_e.cours_id = c.id 
+         LEFT JOIN cours_tag c_t  ON c.id = c_t.cours_id 
+        LEFT JOIN tags t ON t.id = c_t.tag_id',
+        'c.*,GROUP_CONCAT(DISTINCT t.name SEPARATOR ", ") as tags,c_e.status',
         'u.id = ?
         GROUP BY u.id,c.id ORDER BY c_e.status ASC ;', [$id]);
         return $courses;
+
+    }
+
+    public function getInscription($id) {
+     
+        $courses =CRUD::select('cours_etudiant','*','etudiant_id = ?', [$id]);
+        return $courses;
+
+    }
+
+    public function CompletCours($userId,$coursId){
+
+        $cours=[ 
+            'status'=>"complet" 
+        ];
+        CRUD::update('cours_etudiant', $cours, 'etudiant_id = ? and cours_id=?', [$userId,$coursId]);
+
+    }
+
+    public function inscrire($userId,$coursId){
+
+        $cours=[ 
+            'etudiant_id'=>$userId,
+             'cours_id'=>$coursId
+        ];
+        CRUD::insert('cours_etudiant', $cours);
 
     }
 }
 
 $etudiant = new Etudiant;
 
-if(isset($_GET["action"]) && $_GET["action"]=="register"){
+if(isset($_GET["action"])){
+    if($_GET["action"]=="register"){
+        $role=$etudiant->getRole();
+        $etudiant->register($role);
     
-    $role=$etudiant->getRole();
-    $etudiant->register($role);
-    
-}
+    }else if($_GET["action"]=="completCours"){
 
+        $etudiant->CompletCours($_GET["userId"],$_GET["coursId"]);
+        header("Location: http://localhost/Plateforme-de-Cours-en-Ligne-Youdemy/view/singleCours.php?id=".$_GET["coursId"]);
+
+    }else if($_GET["action"]=="inscription"){
+
+        $etudiant->inscrire($_GET["userId"],$_GET["coursId"]);
+        header("Location: http://localhost/Plateforme-de-Cours-en-Ligne-Youdemy/view/singleCours.php?id=".$_GET["coursId"]);
+
+    }
+}
 
 
 
